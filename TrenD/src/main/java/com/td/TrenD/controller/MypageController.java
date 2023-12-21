6ca4e0cc95078ed1d555bf0f16eb1e9a9baf61f3
Meimeidays <nasooak@gmail.com>
2023-12-21 18:05:06 +0900

    mypage jsp 하나로 통일

--- TrenD/src/main/java/com/td/TrenD/controller/MypageController.java
+++ TrenD/src/main/java/com/td/TrenD/controller/MypageController.java
@@ -1,10 +1,15 @@
 package com.td.TrenD.controller;
 
+import com.td.TrenD.service.MypageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/mypage")
 public class MypageController {
-
+    @Autowired
+    private MypageService myservice;
 }
