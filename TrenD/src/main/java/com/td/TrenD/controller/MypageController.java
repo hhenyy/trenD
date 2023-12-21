f38db896cfd9ed7c2ef333e4c25438728093c924
Meimeidays <nasooak@gmail.com>
2023-12-21 16:40:28 +0900

    mypage jsp 생성

--- TrenD/src/main/java/com/td/TrenD/controller/MypageController.java
+++ TrenD/src/main/java/com/td/TrenD/controller/MypageController.java
@@ -1,4 +1,10 @@
 package com.td.TrenD.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class MypageController {
+
 }
