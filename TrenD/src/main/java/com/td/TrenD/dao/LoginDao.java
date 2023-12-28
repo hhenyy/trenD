c3ccb9428c3a89fadf2c7d585b9e53a2192fce5a
DaeHyenon <skinlet@naver.com>
2023-12-28 16:40:46 +0900

    로그인 인서트 그리고 로그인 화면 로그인 시 session userId, userName 추가 및 로그인 아웃시 session 끊음

--- TrenD/src/main/java/com/td/TrenD/dao/LoginDao.java
+++ TrenD/src/main/java/com/td/TrenD/dao/LoginDao.java
@@ -1,9 +1,35 @@
 package com.td.TrenD.dao;
 
-import lombok.RequiredArgsConstructor;
+import com.td.TrenD.model.AgeVO;
+import com.td.TrenD.model.GenderVO;
+import com.td.TrenD.model.LocationVO;
+import com.td.TrenD.model.UserVO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-@RequiredArgsConstructor
-public class LoginDao {
+public interface LoginDao extends JpaRepository<UserVO, String> {
+
+    // JPQL
+    @Query("select a from AgeVO a where a.ageDelYn = 'n' order by a.idx_age")
+    // 기본 findAll 은 기본 메소드 이기 때문에 해당 UserVO만 쓸 수 있고 자기가 임의로 해둔 거는 자신의 내용
+    public List<AgeVO> findSelectAgeAll();
+
+    @Query("select g from GenderVO g where g.genDelYn = 'n' order by g.idx_gender")
+    public List<GenderVO> findSelectGenderAll();
+
+    @Query("select loc from LocationVO loc where loc.locDelYn = 'n' order by loc.idx_location")
+    public List<LocationVO> findSelectLocAll();
+
+    @Query(value = "select count(*) from user where userId = :userId and userDelYn = 'n'" , nativeQuery = true)
+    public int checkId(@Param("userId") String userId);
+
+    @Query(value = "select count(*) from user where userName = :userName and userDelYn = 'n'" , nativeQuery = true)
+    public int checkNick(@Param("userName") String userName);
+
+    public UserVO findByUserId(String userId);
 }
