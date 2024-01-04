e1c396c09e4cb9eb6a28e5b79bc41c673c8b0ab2
JunHyeok Seo <sjhgd107@naver.com>
2024-01-04 09:39:10 +0900

    develop branch merge into junhyeok branch

--- TrenD/src/main/java/com/td/TrenD/dao/TrendReRepository.java
+++ TrenD/src/main/java/com/td/TrenD/dao/TrendReRepository.java
@@ -11,7 +11,4 @@ import java.util.List;
 public interface TrendReRepository extends JpaRepository<TrendReVO, Integer> {
 	@Query("select tr from TrendReVO tr where tr.trNo=:trNo")
 	Page<TrendReVO> findByTrNo(@Param("trNo") int trNo, Pageable pageable);
-
-//	@Query("select count(*) from TrendReVO tr where tr.trReDelYn='n'")
-//	List<TrendReVO> count(@Param("trNo") int trNo);
 }
