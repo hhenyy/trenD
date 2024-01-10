a18c67fa3fdb8c86c60f1c306140dea65751b164
tens124 <tens124@naver.com>
2024-01-10 21:19:34 +0900

    상세통계페이지 restapi 적용 완료

--- TrenD/src/main/java/com/td/TrenD/controller/StatisticsController.java
+++ TrenD/src/main/java/com/td/TrenD/controller/StatisticsController.java
@@ -3,7 +3,9 @@ package com.td.TrenD.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.td.TrenD.model.CategoryVO;
 import com.td.TrenD.model.StatisticsVO;
@@ -108,74 +110,14 @@ public class StatisticsController {
 		index++;
 		}
 	}
+	
+	
+	
+	
 	System.out.println(list);
 	model.addAttribute("json", list);
 	//json 데이터를 뷰 페이지로 전송, 뷰 페이지에선 워드클라우드 라이브러리가 이 자료들을 받아 출력	
 	return "Statistics";
-	}
-	
-	
-	//상세 통계 : 성별,연령,지역에 따른 통계를 표시. 어떤 성별/지역/연령을 가진 사람들이 해당 키워드에 접속했을까?
-	//아 이거 애매하네....어떤 식으로 통계를 출력해야 할지? 성별이야 상관없지만 지역과 연령의 경우 없는 데이터가 존재할 가능성이 큼
-	//특히 지역의 경우 20개 정도의 권역이 설정되어 있기에 대부분의 경우 반 이상은 비어있을 것
-	//걱정하지 않아도 됨. 0이 전송되면 뷰페이지의 통계 라이브러리가 알아서 처리해줌
-	//뷰페이지에선 select를 통해서 연령,성별,지역 등으로 값이 변경될 예정
-	@RequestMapping("/StatisticsDetail")
-	public String StatisticsDetail(Model model, CategoryVO category,String value) {
-		
-		List<Object> list = new ArrayList<Object>();
-		List<Object> finalList = new ArrayList<Object>();
-		
-		//세 개의 sql문을 불러올 것. value값에 따라 다른 sql문
-		//연령,성별,지역 세 개의 값이 select를 통해 전달되어 옴. 이를 받아 활용
-		//1차적으로 cateNm값. 이 값은 워드클라우드의 단어를 클릭 시 자동으로 해당 단어가 등록됨
-		//2차적으로 select의 값. 기본적으론 성별로 설정. 이후 select 내용이 바뀔 때마다 이 컨트롤러가 호출됨
-		if(value == null) {
-			value = "연령";
-		}
-
-		List<String> vlist = new ArrayList<String>();
-		List<String> clist = new ArrayList<String>();
-		
-		if(value.equals("연령")) {
-			vlist = ageService.getList();
-			clist = ageService.countAge(category.getCateNm());
-		}else if(value.equals("성별")) {
-			vlist = genderService.getList();
-			clist = genderService.countGender(category.getCateNm());
-		}else if(value.equals("지역")) {
-			vlist = locationService.getList();
-			clist = locationService.countLocation(category.getCateNm());
-		}else {
-			return "StatisticsDetail";
-		}
-		
-		System.out.println(clist);
-		
-		for(String v : vlist) {
-			int count = Collections.frequency(clist, v);
-			if(count != 0) {
-			list.add("'"+v+"'");
-			list.add(count);   //list 안에는 v와 count 값이 들어가있음
-			System.out.println(list);
-			//1. toArray 메소드는 List를 파라메터(매개변수)로 넘어가는 배열 객체의 size만큼의 배열로 전환한다.
-			//2. 단, 해당 List size가 인자로 넘어가는 배열 객체의 size보다 클때, 해당 List의 size로 배열이 만들어진다.
-			//3. 반대로 해당 List size가 인자로 넘어가는 배열객체의 size보다 작을때는, 인자로 넘어가는 배열객체의 size로 배열이 만들어진다.
-			finalList.add(list);
-			list = new ArrayList<Object>();
-			//System.out.println(Arrays.toString(newList));
-			//newList = new Object[2];
-			//System.out.print(v);
-			//System.out.println(count);
-			}
-		}
-		System.out.println(finalList);
-		//clist 내부에 있는 vlist 내부의 값들 갯수를 센 후, 연관지어 전송
-		
-		model.addAttribute("list", finalList);
-		model.addAttribute("value", value);
-		model.addAttribute("category", category.getCateNm());
-		return "StatisticsDetail";
 	}
 		
 }
